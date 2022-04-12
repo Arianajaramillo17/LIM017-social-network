10e7c6aa39c405af86195e8ee108612f1cb3506c
Angie Mariscal <amariscalp12@gmail.com>
2022-04-12 13:17:01 -0500

    logearse con google

--- src/menu/Login.js
+++ src/menu/Login.js
@@ -1,12 +1,36 @@
-export const Login = () => {
-  const containerLogo = `<p>iniciar secion</p>`;
-  const containerRegistro = `<p>INGRESE SU GMAIL</p>
-                              <input></input>
-                              <p>INGRESE SU CONTRASEÑA</p>
-                              <input></input>`;                            
+import { onNavigate } from "../main.js";
+
+/*export const Login = () => {
+  const containerLogo = `<button id="botonlogout">Haz logout</button> `;
+                        
   const containerIniciarSecion = `<p>
   <button class="button" id="BtnIniciarSecion">INICIAR SECION</button>`;
                             
-  const IniciarSecionConCuenta = containerLogo + containerRegistro + containerIniciarSecion ;
+  const IniciarSecionConCuenta = containerLogo  + containerIniciarSecion ;
   return IniciarSecionConCuenta;
-};
+};*/
+export const Login = (rootElement)=>{
+  const button2 = ` <button id="botonlogout">Haz logout</button>  `  
+rootElement.innerHTML= button2;
+
+const IniciarSecion = document.getElementById('botonlogout'); 
+IniciarSecion.addEventListener('click', function(){
+   onNavigate('/');
+}
+); 
+}
+
+// manejador de eventos para los cambios del estado de autenticación
+/*authService.onAuthStateChanged(function(user) {
+  if (user) {
+    console.log('AuthStateChanged', user)
+    document.getElementById('datosuser').innerHTML = JSON.stringify(user);
+    document.getElementById('botonlogin').style.display = 'none';
+    document.getElementById('botonlogout').style.display = 'block';
+  } else {
+   
+    document.getElementById('datosuser').innerHTML = 'Sin usuario logueado...'
+    document.getElementById('botonlogin').style.display = 'block';
+    document.getElementById('botonlogout').style.display = 'none';
+  }
+});*/
