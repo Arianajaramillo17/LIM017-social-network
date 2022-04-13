8e646cf3dcae933f8d17dff7746efed23b7ddbb8
Arianajaramillo17 <arianajaramillo17@gmail.com>
2022-04-12 19:02:00 -0500

    hasta la madre

--- src/Sesion/NewUser.js
+++ src/Sesion/NewUser.js
@@ -1,19 +1,19 @@
 export const NewUser = (rootElement) => {
-    const containerLogo = `<p>iniciar registro</p>`;
-    const containerRegistro = `<p>GMAIL</p>
+  const containerLogo = `<p>iniciar registro</p>`;
+  const containerRegistro = `<p>GMAIL</p>
 export const NewUser = () => {
-    const containerLogo = `<p class="titleNewUser">iniciar registro</p>`;
-    const containerRegistro = `<p>Subir foto de perfil</p>
-                                <input></input>
-                                <p>Nombre</p>
-                                <input></input>
-                                <p>Correo</p>
-                                <input></input>
-                                <p>Contraseña</p>
-                                <input></input>`;                            
-    const containerCrearCuenta = `<p>
+    const containerLogo = `<p class="titleNewUser"> Iniciar registro </p> `;
+    const containerRegistro = '<p>Subir foto de perfil</p>
+                               <input></input>
+                               <p>Nombre</p>
+                               <input></input>
+                               <p>Correo</p>
+                               <input></input>
+                               <p>Contraseña</p>
+                               <input></input>`;
+  const containerCrearCuenta = `<p>
     <button class="button" id="BotonCrearCuenta">Crear Cuenta</button>
                               `;
-   
-  rootElement.innerHTML = containerLogo+containerRegistro+containerCrearCuenta
-  };
+
+  rootElement.innerHTML = containerLogo + containerRegistro + containerCrearCuenta
+};
