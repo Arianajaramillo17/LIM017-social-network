2a53a297d9a3950df53fd7b4d5dace4a480e30ed
Angie Mariscal <amariscalp12@gmail.com>
2022-04-19 12:24:17 -0500

    funtion total

--- src/Sesion/Home.js
+++ src/Sesion/Home.js
@@ -1,22 +1,13 @@
-//import { onNavigate } from './Login';
-
 export const Home = (rootElement) => {
   const ContainerHome = `<body id="redireccionamiento">
-
-                            <img class="ImageLogo" src="../image/Logo.png"/>
-                            <img class="ImageCharge" src="../image/charge.gif"/>
-
-                          <a href="./Login"><img class="ImageLogo" src="../image/Logo.png" /></a>
-
+                            <a href="./Login"><img class="ImageLogo" src="../image/Logo.png"/></a>
+                            <img class="ImageCharge" src="../image/charge.gif"/> 
+                                                    
                           </body> `;
   rootElement.innerHTML = ContainerHome;
 
   // redireccionamienro
-
-  setTimeout(Redireccionamiento, 2500);
-
  /*setTimeout(Redireccionamiento, 1000);
-
   function Redireccionamiento() {
     onNavigate('/Login');
   }*/
