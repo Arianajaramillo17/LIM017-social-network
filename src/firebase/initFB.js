e2bdce358fbb06b91059e462303cfeeffce7051a
Arianajaramillo17 <arianajaramillo17@gmail.com>
2022-05-09 19:17:29 -0500

    version actualizada auth

--- src/firebase/initFB.js
+++ src/firebase/initFB.js
@@ -1,5 +1,6 @@
-import { initializeApp, getFirestore } from './importFB.js';
+import { initializeApp, getFirestore, getAuth } from './importFB.js';
 import { firebaseConfig } from './configuracionFB.js';
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
+export const auth = getAuth();
