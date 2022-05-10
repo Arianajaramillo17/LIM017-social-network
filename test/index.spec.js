2f871458eaec6265ca6bed7968ee6f0694ea97d2
Arianajaramillo17 <arianajaramillo17@gmail.com>
2022-05-10 10:32:25 -0500

    cambios importfb

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,11 @@
 import { onNavigate } from '../src/main.js';
 
-jest.mock('../src/firebase/mocks.js');
+jest.mock('../src/firebase/importFB.js');
 
 describe('Función onNavigate', () => {
   it('La función onNavigate debe cargar vista Login', () => {
     document.body.innerHTML = '<div id="root"></div>';
-    onNavigate('/Login');
+    onNavigate('/MenuHome');
     const divRoot = document.querySelector('#login-btn');
     expect(divRoot.textContent).toBe('Iniciar sesion');
   });
