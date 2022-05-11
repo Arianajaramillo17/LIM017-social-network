5e39e1f2afade20e70ceef93c3918fc3454767ab
Angie Mariscal <amariscalp12@gmail.com>
2022-05-11 00:57:02 -0500

    test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,42 @@
-import { onNavigate } from '../src/main.js';
+/* import { onNavigate } from '../src/main.js';
 
-jest.mock('../src/firebase/importFB.js');
+jest.mock('../src/firebase/mocks.js');
 
 describe('Función onNavigate', () => {
-  it('La función onNavigate debe cargar vista Login', () => {
+  it('vista login', () => {
     document.body.innerHTML = '<div id="root"></div>';
     onNavigate('/Login');
     const divRoot = document.querySelector('#login-btn');
     expect(divRoot.textContent).toBe('Iniciar sesion');
+  });
+});
+import {
+  NewUser,
+} from '../src/Sesion/NewUser.js';
+
+jest.mock('../src/firebase/importFB.js');
+
+describe('Autenticar', () => {
+  it('debería ser una funciónn que crea cuenta con google', () => {
+    expect(typeof NewUser).toBe('function');
+  });
+});  */
+
+import { NewUser } from '../src/Sesion/NewUser.js';
+
+jest.mock('../src/firebase/importFB.js');
+
+describe('NewUser()', () => {
+  it('El botón de Registrarse se encuentra en register()', () => {
+    const result = NewUser();
+    const sighUp = result.querySelector('#sighUp');
+    sighUp.dispatchEvent(new Event('click'));
+    expect(sighUp.textContent).toBe('REGISTRARSE');
+  });
+});
+
+describe('Autenticar', () => {
+  it('crea una cuenta ', () => {
+    expect(typeof NewUser).toBe('function');
   });
 });
